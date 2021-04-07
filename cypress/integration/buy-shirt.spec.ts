407f30c9ea7e1d0bb859ae01b066f6816dd371bc
Antonieta <antonietaavellaneda@gmail.com>
2021-04-07 14:13:13 -0500

    Delete comments

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -21,49 +21,37 @@ describe("Buy a t-shirt", () => {
     it("then the t-shirt should be bought", () => {
 
         menuContentPage.visitMenuContentPage()
-        //cy.visit("http://automationpractice.com/");
         menuContentPage.goToTShirtMenu()
-        //cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
 
         //product
         productListPage.goAddToCart()
-        //cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
         productListPage.goThisButton()
-        //cy.get("[style*=\"display: block;\"] .button-container > a").click();
 
         //shopping
         shoppingCartPage.Cart()
-        //cy.get(".cart_navigation span").click();
 
         //login
         loginPage.insertMail("aperdomobo@gmail.com")
-        //cy.get("#email").type("aperdomobo@gmail.com");
         loginPage.insertPassword("WorkshopProtractor")
-        //cy.get("#passwd").type("WorkshopProtractor");
         loginPage.goSubmit()
-        //cy.get("#SubmitLogin").click();
+
 
         //shipping
         shippingStepPage.goCartNavigation()
-        //cy.get(".cart_navigation span").click();
+
 
         //address
         addressStepPage.ReviewAddress()
-        //cy.get(".checker").click();
         addressStepPage.ConfirmAddress()
-        //cy.get(".cart_navigation span").click();
 
         //payment
         paymentStepPage.PaymentType()
-        //cy.get("a.bankwire").click();
+
 
         paymentStepPage.ConfirmPayment()
-        //cy.get(".cart_navigation span").click();
+
 
         paymentStepPage.ConfirmationMessage()
             .should("have.text", "Your order on My Store is complete.");
-
-        //cy.get("#center_column > div > p > strong")
-        //    .should("have.text", "Your order on My Store is complete.");
     });
 })
